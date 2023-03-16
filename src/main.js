e8e6623e7f546d0db39bb5ef5f9b1d13cbd6393f
SagaBryel <gl.paschoal3@gmail.com>
2023-03-16 17:11:01 -0300

    change map_left

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Phaser from './lib/phaser.js'
 
 import Scene_center from './scenes/map1_center/Scene_center.js'
 import Scene_up from './scenes/map2_up/Scene_up.js'
-
+import Scene_left from './scenes/map5/Scene_left.js'
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [Scene_up, ],
+    scene: [Scene_left, ],
     physics: {
         default: 'arcade',
         arcade: {
