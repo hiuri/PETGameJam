8f8b518138ecf4c8de67987fa232c123d7fdeff2
Hiuri Carriço Liberato <hiuri3112@hotmail.com>
2023-03-15 23:51:40 -0300

    changes map_up

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Phaser from './lib/phaser.js'
 
 import Scene_center from './scenes/map1_center/Scene_center.js'
+import Scene_up from './scenes/map2_up/Scene_up.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [Scene_center, ],
+    scene: [Scene_up, ],
     physics: {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 1000
+                y: 0
             },
             debug: true
         }
---
+++ src/scenes/map2_up/Scene_up.js
@@ -0,0 +1,41 @@
+import Phaser from '../../lib/phaser.js'
+
+
+export default class Scene_up extends Phaser.Scene{
+    constructor(){
+        super('scene_up');
+    }
+
+    preload(){
+        this.load.image("background", "./src/scenes/map2_up/img/map_up.jpg");
+        this.load.spritesheet("dragon", "./src/scenes/map2_up/img/dragon.png",{
+            frameWidth: 400,
+            frameHeight: 400,
+          });
+    }
+    
+    create(){
+    
+        // adicionando o background
+        this.background = this.add.tileSprite(400,300,800,600,"background");
+
+        //adicionando o dragao no mapa
+        this.dragons = this.physics.add.group()
+        this.dragons.create(
+            400,100, "dragon"
+        ).setScale(0.3)
+
+        this.anims.create({
+            key: "dragon_idle",
+            frames: this.anims.generateFrameNumbers("dragon", {start:6, end:8}),
+            frameRate: 60,
+            repeat:-1
+        });
+
+        this.dragons.children.iterate((dragon)=>{dragon.play("dragon_idle")})
+    }
+    
+    update(){
+
+    }
+}
