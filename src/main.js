5bc6c528c7d9085a0d1dd7f08b7c90ffd2ff5afc
Hiuri Carriço Liberato <hiuri3112@hotmail.com>
2023-03-17 13:23:47 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import Phaser from './lib/phaser.js'
 import Scene_center from './scenes/map1_center/Scene_center.js'
 import Scene_up from './scenes/map2_up/Scene_up.js'
 import Scene_left from './scenes/map5_left/Scene_left.js'
-import Scene_right from '.scenes/map3_right/Scene_right'
+import Scene_right from './scenes/map3_right/Scene_right.js'
 
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [Scene_right, ],
+    scene: [Scene_left, ],
     physics: {
         default: 'arcade',
         arcade: {
