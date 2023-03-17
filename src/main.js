7a0fcb8aa21d7d113a97379675d4ba8524b5e965
SagaBryel <gl.paschoal3@gmail.com>
2023-03-17 13:24:55 -0300

    changeagain

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [Scene_right, ],
+    scene: [Scene_left, ],
     physics: {
         default: 'arcade',
         arcade: {
